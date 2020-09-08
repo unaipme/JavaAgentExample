678c1e1345cc699907d1384a5f1fe531efc63eb2
Unai Perez <unaipme@gmail.com>
2020-09-08 11:32:08 +0200

    Name of agent thread changed

--- src/main/java/Agent.java
+++ src/main/java/Agent.java
@@ -6,6 +6,7 @@ public class Agent {
 
     public static void agentmain(String agentArgs, Instrumentation inst) {
         // instrumentation = inst;
+        Thread.currentThread().setName("THIS IS THE AGENT THREAD");
         for (Thread t : Thread.getAllStackTraces().keySet()) {
             System.out.println(t.getName());
         }
